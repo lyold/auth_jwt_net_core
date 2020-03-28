33c822dec2aa06f8c8c9750564e15099ad13a5e9
lyold <r.almeidagustavo@gmail.com>
2020-03-27 23:43:01 -0300

    Primeiro commit do projeto de autenticação

---
+++ RegisterPerson.API/RegisterPerson.API/Controllers/PersonController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegisterPerson.Domain.Model.Entities;
+using RegisterPerson.Domain.Services.Interfaces;
+
+namespace RegisterPerson.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            this._personService = personService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult<string> Get(int id)
+        {
+            try
+            {
+                return Ok(_personService.Find(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult Post(Person person)
+        {
+            try
+            {
+                return Ok(_personService.Create(person));
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult Put(Person person)
+        {
+            try
+            {
+                return Ok(_personService.Update(person));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _personService.Delete(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ RegisterPerson.API/RegisterPerson.API/Startup.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RegisterPerson.API.Services.Context.Implementation;
+using RegisterPerson.DataAccess.Abstract.Entities;
+using RegisterPerson.DataAccess.SqlServer.Context;
+using RegisterPerson.Domain.Services.Implementation;
+using RegisterPerson.Domain.Services.Interfaces;
+
+namespace RegisterPerson.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            #region Registra o banco de dados
+
+            var connection = Configuration["SQLServerConnection:SQLServerConnectionString"];
+            services.AddDbContext<SQLServerContext>(options => options.UseSqlServer(connection));
+
+            #endregion
+
+            #region  Registra as dependências
+
+            services.AddScoped<IPersonService, PersonService>();
+            services.AddScoped<IPersonServiceSqlServer, PersonServiceSqlServer>();
+            
+            #endregion
+
+            #region Registra o Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "RestFul API with .NET Core 2.0",
+                    Version = "v1"
+                });
+            });              
+
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            #region Configurações do Swagger
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(s => {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
+            #endregion
+            
+            app.UseHttpsRedirection();
+            app.UseMvc(routes=> 
+            {
+                routes.MapRoute(
+                        name: "Default API",
+                        template: "{controllern=Values}/{id?}"
+                    );
+            });
+            
+
+        }
+    }
+}
